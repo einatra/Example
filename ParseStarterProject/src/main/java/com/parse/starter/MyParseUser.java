dc571045df4e34147c22474749170fc872f9dbbe
einatra <einat_8@yahoo.com>
2015-09-30 21:29:40 +0300

    Initial commit

---
+++ ParseStarterProject/src/main/java/com/parse/starter/MyParseUser.java
@@ -0,0 +1,16 @@
+package com.parse.starter;
+
+import com.parse.ParseClassName;
+import com.parse.ParseUser;
+
+@ParseClassName("MyParseUser")
+/**
+ * Created by Einat on 30/09/2015.
+ */
+public class MyParseUser extends ParseUser {
+
+    public MyParseUser() {
+    }
+
+
+}
---
+++ ParseStarterProject/src/main/java/com/parse/starter/SignUp.java
@@ -0,0 +1,81 @@
+package com.parse.starter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignUp extends AppCompatActivity {
+
+    EditText userN, pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        userN = (EditText) findViewById(R.id.signEditUser);
+        pass = (EditText) findViewById(R.id.signEditPass);
+
+
+        findViewById(R.id.signBtnDone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addUser();
+            }
+        });
+    }
+
+    public MyParseUser addUser(){
+        MyParseUser user = new MyParseUser();
+        user.setUsername(userN.getText().toString());
+        user.setPassword(pass.getText().toString());
+        user.setEmail("email@example.com");
+
+// other fields can be set just like with ParseObject
+        user.put("phone", "650-555-0000");
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(com.parse.ParseException e) {
+                if (e == null) {
+                    Toast.makeText(SignUp.this, "user created", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SignUp.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                }
+            }
+        });
+        return user;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sign_up, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
